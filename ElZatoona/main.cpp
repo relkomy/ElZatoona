4de1756c77fa06236ba0e84de7aba42a3edac9d9
VNET\relkomy1 <ramy.elkomy@valeo.com>
2019-08-07 00:52:35 +0200

    initial commit for ElZatoona project

---
+++ ElZatoona/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    std::cout << "Welcome to ElZatoona!" << std::endl;
+
+    return a.exec();
+}
