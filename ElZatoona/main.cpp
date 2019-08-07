b9a98e1481ad678176ac8b7933fd8927cd572ab4
Alaa ABDALLAH <alaa.abdallah@valeo.com>
2019-08-07 14:08:25 +0200

    update

--- ElZatoona/main.cpp
+++ ElZatoona/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     std::cout << "Welcome to ElZatoona!" << std::endl;
+	std::cout << "Hii all!" << std::endl;
 
     return a.exec();
 }
