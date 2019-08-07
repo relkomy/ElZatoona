0de4de9c1de98140c6defee7774f618c686862f4
VNET\relkomy1 <ramy.elkomy@valeo.com>
2019-08-07 13:58:00 +0200

    commit 22

--- ElZatoona/main.cpp
+++ ElZatoona/main.cpp
@@ -7,5 +7,8 @@ int main(int argc, char *argv[])
 
     std::cout << "Welcome to ElZatoona!" << std::endl;
 
+    std::cout << "change 1" << std::endl;
+    std::cout << "change 2" << std::endl;
+
     return a.exec();
 }
